0f95a85a7e37de25e4a1d5a3954704971e037790
jia wei <jw397@126.com>
2021-08-18 19:29:57 +0800

    fix: 撤销缓存

--- src/lib/serve-dir.ts
+++ src/lib/serve-dir.ts
@@ -1,19 +1,19 @@
 import { createServer, Server } from "http";
 import serveHandler from "serve-handler";
 
-let serverPromise: Promise<any> | null;
-let serverCount = 0;
+// let serverPromise: Promise<any> | null;
+// let serverCount = 0;
 /**
  * Serve a directory on a random port using a HTTP server and the serve-handler package.
  *
  * @returns a promise that resolves with the server instance once the server is ready and listening.
  */
 export const serveDirectory = async (basedir: string, port: number) => {
-	serverCount++;
-	if (serverPromise) {
-		return serverPromise;
-	}
-	serverPromise = new Promise<Server>((resolve) => {
+	// serverCount++;
+	// if (serverPromise) {
+	// 	return serverPromise;
+	// }
+	const serverPromise = new Promise<Server>((resolve) => {
 		const server = createServer(async (request, response) =>
 			serveHandler(request, response, { public: basedir })
 		);
@@ -27,11 +27,11 @@ export const serveDirectory = async (basedir: string, port: number) => {
  * Close the given server instance asynchronously.
  */
 export const closeServer = async (server: Server) => {
-	if (serverCount > 1) {
-		serverCount--;
-		return;
-	}
-	serverPromise = null;
-	serverCount = 0;
+	// if (serverCount > 1) {
+	// 	serverCount--;
+	// 	return;
+	// }
+	// serverPromise = null;
+	// serverCount = 0;
 	return new Promise((resolve) => server.close(resolve));
 };
