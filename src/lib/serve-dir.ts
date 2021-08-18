2df5c37da8ce0a1a07cbbe91a621f3ea20f36fd6
jia wei <jw397@126.com>
2021-08-18 14:32:50 +0800

    feat: 支持同时多个打印

--- src/lib/serve-dir.ts
+++ src/lib/serve-dir.ts
@@ -1,20 +1,38 @@
-import { createServer, Server } from 'http';
-import serveHandler from 'serve-handler';
-import { Config } from './config';
+import { createServer, Server } from "http";
+import serveHandler from "serve-handler";
+import { Config } from "./config";
 
+let serverPromise: Promise<any> | null;
+let serverCount = 0;
 /**
  * Serve a directory on a random port using a HTTP server and the serve-handler package.
  *
  * @returns a promise that resolves with the server instance once the server is ready and listening.
  */
-export const serveDirectory = async ({ basedir, port }: Config) =>
-	new Promise<Server>((resolve) => {
-		const server = createServer(async (request, response) => serveHandler(request, response, { public: basedir }));
+export const serveDirectory = async ({ basedir, port }: Config) => {
+  serverCount++;
+  if (serverPromise) {
+    return serverPromise;
+  }
+  serverPromise = new Promise<Server>((resolve) => {
+    const server = createServer(async (request, response) =>
+      serveHandler(request, response, { public: basedir })
+    );
 
-		server.listen(port, () => resolve(server));
-	});
+    server.listen(port, () => resolve(server));
+  });
+  return serverPromise;
+};
 
 /**
  * Close the given server instance asynchronously.
  */
-export const closeServer = async (server: Server) => new Promise((resolve) => server.close(resolve));
+export const closeServer = async (server: Server) => {
+  if (serverCount > 1) {
+    serverCount--;
+    return;
+  }
+  serverPromise = null;
+  serverCount = 0;
+  return new Promise((resolve) => server.close(resolve));
+};
